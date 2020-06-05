80871ed48c6812675c6bce50905a304fddcce88f
viksatheboss <61952531+viksatheboss@users.noreply.github.com>
2020-06-05 07:57:22 +0200

    Add files via upload

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskProject.Models;
+
+namespace TaskProject.Controllers
+{
+    public class CompaniesController : Controller
+    {
+        
+        // GET: Companies
+        public ActionResult Index(string search = "")
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            ViewBag.search = search;
+            List<Company> companies = _context.Companies.Where(temp => temp.CompanyName.Contains(search)).ToList();
+            
+            return View();
+        }
+
+        public ActionResult Details(long id)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            Company c = _context.Companies.Where(temp => temp.Id == id).FirstOrDefault();
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            ViewBag.Companies = _context.Companies.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Company c)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            _context.Companies.Add(c);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            Company company = _context.Companies.Where(temp => temp.Id == id).FirstOrDefault();
+            return View(company);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Company c)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            Company company = _context.Companies.Where(temp => temp.Id == c.Id).FirstOrDefault();
+            company.CompanyName = c.CompanyName;
+            company.ContactNumber = c.ContactNumber;
+            company.Address = c.Address;
+            company.Id = c.Id;
+            
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Companies");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            Company company = _context.Companies.Where(temp => temp.Id == id).FirstOrDefault();
+            return View(company);
+        }
+        public ActionResult Delete(int id, Company c)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            Company existingVehicle = _context.Companies.Where(temp => temp.Id == id).FirstOrDefault();
+            _context.Companies.Remove(existingVehicle);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Companies");
+        }
+
+
+    }
+}
---
+++ Controllers/PeopleController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services.Description;
+using TaskProject.Models;
+
+namespace TaskProject.Controllers
+{
+    public class PeopleController : Controller
+    {
+        
+
+        // GET: People
+        public ActionResult Index(string search = "" )
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            ViewBag.search = search;
+            List<People> people = _context.People.Where(temp => temp.FirstName.Contains(search)).ToList();
+            return View();
+        }
+
+        public ActionResult Details(long id)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            People p = _context.People.Where(temp => temp.Id == id).FirstOrDefault();
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            ViewBag.People = _context.People.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(People p)
+        {
+
+            ApplicationDbContext _context = new ApplicationDbContext();
+            _context.People.Add(p);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+
+
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            People people = _context.People.Where(temp => temp.Id == id).FirstOrDefault();
+            return View(people);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(People p)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            People people = _context.People.Where(temp => temp.Id == p.Id).FirstOrDefault();
+            people.FirstName = p.FirstName;
+            people.LastName = p.LastName;
+            people.MobileNumber = p.MobileNumber;
+            people.Address = p.Address;
+            people.Id = p.Id;
+
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "People");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            People people = _context.People.Where(temp => temp.Id == id).FirstOrDefault();
+            return View(people);
+        }
+        public ActionResult Delete(int id, People p)
+        {
+            ApplicationDbContext _context = new ApplicationDbContext();
+            People people = _context.People.Where(temp => temp.Id == id).FirstOrDefault();
+            _context.People.Remove(people);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "People");
+        }
+    }
+}
